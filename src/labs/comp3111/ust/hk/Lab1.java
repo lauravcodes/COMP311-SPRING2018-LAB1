28dafbee85ef9cbc9835421d90169bbc074c0371
lauri <lauri@DESKTOP-9UNLDCB>
2018-02-13 11:39:20 +0800

    Added a congratulations message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -3,6 +3,7 @@ package labs.comp3111.ust.hk;
 public class Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Lab 1: Introduction to Git and GitHub");
+		System.out.println("Congratulations, you learned the GitHub basics!");
 	}
 
 }
